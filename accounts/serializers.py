056bcb4972e0ed0e9501e37af469f780656fe92c
Davee Sok <daveesok@gmail.com>
2021-11-11 10:09:57 -0600

    had to add ability to add first and last name

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,15 +14,29 @@ class RegisterSerializer(serializers.ModelSerializer):
             "password": {"write_only": True},
         }
 
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(email=validated_data["email"], password=validated_data["password"])
+    def create(self, validated_data, **extra_fields):
+        user = CustomUser.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            **extra_fields
+        )
         return user
+
+
+#
+#
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = "__all__"
+
+
+#
+#
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
