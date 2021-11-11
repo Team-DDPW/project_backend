c3a3f29d4ab334cd5c5822bce1a7f2a381cc6e2a
Davee Sok <daveesok@gmail.com>
2021-11-10 20:06:25 -0600

    changed package

--- packagerequest/admin.py
+++ packagerequest/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import PackageRequest
 
 
-@admin.register(PackageRequest)
-class AdminForPackageRequest(admin.ModelAdmin):
-    list_display = ("title", "author", "created_at")
+# @admin.register(PackageRequest)
+# class AdminForPackageRequest(admin.ModelAdmin):
+#     list_display = ("title", "author", "created_at")
 
 
-# admin.site.register(AnotherModel)
+admin.site.register(PackageRequest)
